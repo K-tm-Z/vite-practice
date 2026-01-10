7950c38f5858283888a2b32245ad8607a2b24a01
Peter Nguyen <peternguyen.officiel@gmail.com>
2026-01-09 20:01:37 -0500

    Trial #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import but_why from './assets/but_why.gif'
 
 function App() {
   return (
@@ -7,62 +8,67 @@ function App() {
       <Navbar />
       <h1 className="test">Introduction</h1>
       <p>This is a Vite project setup with React.</p>
-      <div className="section">
-        <div className="box">
-          <div className="card">Card 1
-            <div>
-              This is some content inside Card 1.
-            </div>
+      <div className='section-wrapper'>
+        <div className="section">
+          <div className="box">
+            <img src={but_why} alt="Meme" className="meme" />
           </div>
-          <div className="card">Card 2
-            <div>
-              This is some content inside Card 2.
-            </div>
-          </div>
-          <div className="card">Card 3
-            <div>
-              This is some content inside Card 3.
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="section">
-        <div className="box">
-          <div className="card">Card 1
-            <div>
-              This is some content inside Card 1.
-            </div>
-          </div>
-          <div className="card">Card 2
-            <div>
-              This is some content inside Card 2.
-            </div>
-          </div>
-          <div className="card">Card 3
-            <div>
-              This is some content inside Card 3.
-            </div>
+          <div className='box'>
+            <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt 
+              ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, <em>quis 
+              nostrud exercitation ullamco laboris 
+              nisi ut aliquip ex ea commodo 
+              consequat.</em> Duis aute irure 
+              dolor in reprehenderit in voluptate 
+              velit esse cillum dolore eu fugiat nulla pariatur. 
+              Excepteur sint occaecat cupidatat non proident, 
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
           </div>
         </div>
-      </div>
 
-      <div className="section">
-        <div className="box">
-          <div className="card">Card 1
-            <div>
-              This is some content inside Card 1.
-            </div>
+        <div className="section section--reverse">
+          <div className='box'>
+            <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt 
+              ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, <em>quis 
+              nostrud exercitation ullamco laboris 
+              nisi ut aliquip ex ea commodo 
+              consequat.</em> Duis aute irure 
+              dolor in reprehenderit in voluptate 
+              velit esse cillum dolore eu fugiat nulla pariatur. 
+              Excepteur sint occaecat cupidatat non proident, 
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
           </div>
-          <div className="card">Card 2
-            <div>
-              This is some content inside Card 2.
-            </div>
+          <div className="box">
+            <img src={but_why} alt="Meme" className="meme" />
           </div>
-          <div className="card">Card 3
-            <div>
-              This is some content inside Card 3.
-            </div>
+        </div>
+
+        <div className="section">
+          <div className="box">
+            <img src={but_why} alt="Meme" className="meme" />
+          </div>
+          <div className='box'>
+            <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt 
+              ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, <em>quis 
+              nostrud exercitation ullamco laboris 
+              nisi ut aliquip ex ea commodo 
+              consequat.</em> Duis aute irure 
+              dolor in reprehenderit in voluptate 
+              velit esse cillum dolore eu fugiat nulla pariatur. 
+              Excepteur sint occaecat cupidatat non proident, 
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
           </div>
         </div>
       </div>
